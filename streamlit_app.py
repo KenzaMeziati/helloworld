57ce3c468073de76b3f349d65f910bce3100bc51
KenzaMeziati <96596321+KenzaMeziati@users.noreply.github.com>
2022-04-09 23:04:59 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ df2=pd.DataFrame(
     columns=['a','b','c']
     )
 c=alt.Chart(df2).mark_circle().encode(
-    x='a', y='b', size='c',color='c', tooltip=['a','b','c'], cmap='summer',
+    x='a', y='b', size='c',color='c', tooltip=['a','b','c'],
     )
 st.write(c)
 
